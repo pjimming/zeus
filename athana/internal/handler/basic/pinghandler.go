c227fe959ab1852c7b7b6bf5f24b51dcf00eb986
pan <2556323541@qq.com>
2023-09-10 00:17:17 +0800

    [INIT] add athana app

---
+++ athana/internal/handler/basic/pinghandler.go
@@ -0,0 +1,16 @@
+package basic
+
+import (
+	"net/http"
+
+	"github.com/pjimming/zeus/athana/internal/svc"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func PingHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("pong"))
+		httpx.Ok(w)
+	}
+}
